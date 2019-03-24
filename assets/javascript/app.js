4ab50cfb443223e7d0a86f5c7bf9b91f9d82253d
Darren Steverson <dcs194@scarletmail.rutgers.edu>
2019-03-24 00:01:12 -0400

    adding questions

---
+++ assets/javascript/app.js
@@ -0,0 +1,37 @@
+var questions = [
+{
+    "question": "How many NBA Championships do the Bulls have?",
+    "option1": "3",
+    "option2": "8",
+    "option3": "6",
+    "answer": "2"
+},
+{
+    "question": "What team plays in Houston?",
+    "option1": "Rockets",
+    "option2": "Pistons",
+    "option3": "Bucks",
+    "answer": "Spurs"
+},
+{
+    "question": "Which player duo  makes up 'The Splash Brothers'?",
+    "option1": "",
+    "option2": "",
+    "option3": "",
+    "answer": "",
+},
+{
+    "question": "",
+    "option1": "",
+    "option2": "",
+    "option3": "",
+    "answer": "",
+},
+{
+    "question": "",
+    "option1": "",
+    "option2": "",
+    "option3": "",
+    "answer": "",
+},
+}]
