cdf453676c614f796153a172d2b3d5b1a4afcefd
darren steverson <dcs194@scarletmail.rutgers.edu>
2019-03-25 16:56:50 -0400

    fixed questions format; inputing questions into quiz functionality

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -7,49 +7,83 @@ function myTimer() {
     sec--;
     if (sec == -1) {
         clearInterval(time);
-        alert("Times Out!");
+        alert("Times Up!");
     }
 }
 
 // Questions and Answers
 var quizQuestions = [
 {
-    "question": "How many NBA Championships do the Bulls have?",
-    "options": ["3","8","6"],
-    "answer": "6"
+    question: "How many NBA Championships do the Bulls have?",
+    options: {
+        option1: "3",
+        option2: "8",
+        option3: "6",
+        option4: "16"},
+    answer: "6"
 },
 {
-    "question": "What team plays in Houston?",
-    "options": ["Rockets","Bucks","Pistons"],
-    "answer": "Rockets"
+    question: "What team plays in Houston?",
+    options: {
+        option1: "Rockets",
+        option2: "Bucks",
+        option3: "Pistons",
+        option4: "Spurs"},
+    answer: "Rockets"
 },
 {
-    "question": "Which player duo makes up 'The Splash Brothers'?",
-    "options": ["Stephen Curry and Klay Thompson","Damian Lillard and Cj McCollum","James Harden and Chris Paul"],
-    "answer": "Stephen Curry and Klay Thompson",
+    question: "Which backcourt duo makes up 'The Splash Brothers'?",
+    options: {
+        option1: "Stephen Curry and Klay Thompson",
+        option2: "Damian Lillard and Cj McCollum",
+        option3: "James Harden and Chris Paul",
+        option4: "John Wall and Bradley Beal"},
+    answer: "Stephen Curry and Klay Thompson",
 },
 {
-    "question": "Which team has LeBron James not played on?",
-    "options": ["Cleveland Caviliers","Miami Heat","Brooklyn Nets"],
-    "answer": "Brooklyn Nets",
+    question: "Which team has LeBron James not played on?",
+    options: {
+        option1:"Cleveland Caviliers",
+        option2:"Miami Heat",
+        option3:"Brooklyn Nets",
+        option4:"LA Lakers"},
+    answer: "Brooklyn Nets",
 },
 {
-    "question": "Which is not a basketball position? ",
-    "options": ["Center","Gaurd","Shooter"],
-    "answer": "Shooter",
+    question: "Which is not a basketball position? ",
+    options: {
+        option1:"Center",
+        option2:"Gaurd",
+        option3:"Forward",
+        option4:"Shooter"},
+    answer: "Shooter",
 },
 ]
+
+
+
 // To display elements on screen
-var quizContainer = document.getElementById('quiz');
-var resultsContainer = document.getElementById('results');
+var quizText = document.getElementById('quiz');
+var resultsText = document.getElementById('results');
 var submitButton = document.getElementById('submit');
 
 
-// Quiz Content
-function nbaquestions (nbaquestions){
+// Hold all Quiz Content
+function quizContent (questions,quizText, resultsText, submitButton){
 
-    for (i = 0; i < nbaquestions.length; i++){
-        var quizQuestions = nbaquestions [i];
-        var answer = quizQuestions.length;
+    function viewQuestions (questions, quizText){
+
+        var output = [];
+        var options;
+
+
     }
-    ;}
+
+    viewQuestions(questions, quizText)
+
+
+
+
+
+
+}
